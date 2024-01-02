8a7bc5eaec8f7bdab4781f94a6d2e17f23071441
mouda77 <mmustafa8@gmail.com>
2024-01-03 00:02:14 +0200

    Update script1.sql

--- scripts/script1.sql
+++ scripts/script1.sql
@@ -1,10 +1,10 @@
 DROP TABLE student_data2 ;
 
-CREATE TABLE student_data2 (
-             student_id int primary key,
-             student_name varchar(25),
-             student_age int,
-             gender char (1),
-             DOA date,
-             city_name varchar(20)
-);
+--CREATE TABLE student_data2 (
+  --           student_id int primary key,
+    --         student_name varchar(25),
+      --       student_age int,
+        --     gender char (1),
+          --   DOA date,
+            -- city_name varchar(20)
+-- );
