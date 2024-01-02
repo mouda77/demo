340dd1091b5959d29d59c50901514ab39d7fa7f3
mouda77 <mmustafa8@gmail.com>
2024-01-03 00:07:10 +0200

    Update script1.sql

--- scripts/script1.sql
+++ scripts/script1.sql
@@ -1,10 +1,8 @@
-select student_id  from student_data2 ;
-
---CREATE TABLE student_data2 (
-  --           student_id int primary key,
-    --         student_name varchar(25),
-      --       student_age int,
-        --     gender char (1),
-          --   DOA date,
-            -- city_name varchar(20)
--- );
+CREATE TABLE student_data2 (
+             student_id int primary key,
+             student_name varchar(25),
+             student_age int,
+             gender char (1),
+             DOA date,
+             city_name varchar(20)
+ );
