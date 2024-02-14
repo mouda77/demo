5350e841035fa9a3766e267a50929d37ef89d778
GitHub Action <action@github.com>
2024-02-14 20:56:43 +0200

    Append content from deploy1.sql

--- schema.sql
+++ schema.sql
@@ -5,3 +5,4 @@ CREATE TABLE `customers3` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY 
 -- Create "customers5" table
 CREATE TABLE `customers5` (`id` int NOT NULL, `name` varchar(100) NULL, `text` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
 CREATE TABLE  (uid=1001(ouda) gid=1001(ouda) groups=1001(ouda),10(wheel) int NOT NULL,  varchar(100) NULL,  varchar(100) NULL, PRIMARY KEY (uid=1001(ouda) gid=1001(ouda) groups=1001(ouda),10(wheel))) CHARSET latin1 COLLATE latin1_swedish_ci;
+CREATE TABLE  (uid=1001(ouda) gid=1001(ouda) groups=1001(ouda),10(wheel) int NOT NULL,  varchar(100) NULL,  varchar(100) NULL, PRIMARY KEY (uid=1001(ouda) gid=1001(ouda) groups=1001(ouda),10(wheel))) CHARSET latin1 COLLATE latin1_swedish_ci;
