4d56cc4af0103924a149d86eeb2e7dfb87a0f7d9
mouda77 <action@github.com>
2024-02-15 10:44:01 +0200

    commit schema file

--- schema.sql
+++ schema.sql
@@ -1,4 +1,2 @@
 -- Create "customers2" table
 CREATE TABLE `customers2` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
--- Create "myloa" table
-CREATE TABLE `myloa` (`id` int NOT NULL, `name` varchar(100) NULL, `texta` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
