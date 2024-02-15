a8d7e8c9bb0b2334c46866fe1e4246f752c290c3
mouda77 <mmustafa8@gmail.com>
2024-02-15 12:39:04 +0200

    Delete schema.sql

--- schema.sql
+++
@@ -1,5 +0,0 @@
--- Create "customers2" table
-CREATE TABLE `customers2` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
--- Create "myloa" table
-CREATE TABLE `myloa` (`id` int NOT NULL, `name` varchar(100) NULL, `texta` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
-CREATE TABLE myloa (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
