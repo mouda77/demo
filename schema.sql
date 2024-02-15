99b4bdd53bc0ed7f86d49db5d623059653b8df11
GitHub Action <action@github.com>
2024-02-15 10:57:05 +0200

    Append content from deploy1.sql to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 -- Create "myloa" table
 CREATE TABLE `myloa` (`id` int NOT NULL, `name` varchar(100) NULL, `texta` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
+CREATE TABLE myloa (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
