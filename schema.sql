b227ee85c42269dd2ad74f423b285d0bfdfe19f8
mouda77 <action@github.com>
2024-02-15 10:31:55 +0200

    Append content from deploy1.sql

--- schema.sql
+++ schema.sql
@@ -5,3 +5,4 @@ CREATE TABLE `customers3` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY 
 -- Create "customers5" table
 CREATE TABLE `customers5` (`id` int NOT NULL, `name` varchar(100) NULL, `text` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
 CREATE TABLE myloa (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
+CREATE TABLE myloa (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
