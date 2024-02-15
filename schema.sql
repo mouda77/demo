545939558333926279e5f2b36bce725e88dd38f0
GitHub Action <action@github.com>
2024-02-15 11:49:36 +0200

    Append content from deploy1.sql to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 -- Create "customers2" table
 CREATE TABLE `customers2` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
+CREATE TABLE myloa (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
