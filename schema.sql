af908b586f9811ec6a5b6733111b572c4ac128aa
GitHub Action <action@github.com>
2024-04-18 22:11:26 +0200

    Save updated schema.sql after deployment

---
+++ schema.sql
@@ -0,0 +1,5 @@
+-- Create "customers2" table
+CREATE TABLE `customers2` (`id` int NOT NULL, `name` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
+-- Create "myloa" table
+CREATE TABLE `myloa` (`id` int NOT NULL, `name` varchar(100) NULL, `texta` varchar(100) NULL, PRIMARY KEY (`id`)) CHARSET latin1 COLLATE latin1_swedish_ci;
+CREATE TABLE mylo1 (id int NOT NULL, name varchar(100) NULL, texta varchar(100) NULL, PRIMARY KEY (id)) CHARSET latin1 COLLATE latin1_swedish_ci;
