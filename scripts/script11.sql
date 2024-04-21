bcf75f69db53a5679295667d5727af179fd31a46
mouda77 <mmustafa8@gmail.com>
2024-04-21 14:53:37 +0200

    Delete scripts directory

--- scripts/script11.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE student_data2 (
-           student_id int primary key,
-           student_name varchar(25),
-             student_age int,
-             gender char (1),
-             DOA date,
-             city_name varchar(20)
- );
